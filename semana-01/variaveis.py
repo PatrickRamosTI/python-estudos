16256ff5f40f8eb3abfdd2d50b3bdd0f4ea08e67
Patrick Ramos <patrickramosti@gmail.com>
2026-09-20 15:25:40 -0300

    Variáveis e operadores

---
+++ semana-01/variaveis.py
@@ -0,0 +1,18 @@
+nome = "Patrick"
+idade = 29
+altura = 1.67
+é_estudante = True
+
+"""
+- Os nomes das variáveis só podem conter letras (a-z, A-Z),
+números (0-9) e sublinhados (_). Não podem começar com um número.
+
+- O Python diferencia maiúsculas de minúsculas,
+então nome e Nome são variáveis diferentes.
+
+- Não se pode usar palavras-chave reservadas do Python como nomes de variáveis
+(por exemplo, if, else, for, while, etc.).
+
+- Recomenda-se usar nomes descritivos para as variáveis,
+que indiquem claramente seu propósito: nome, idade, total_vendas, etc.
+"""
